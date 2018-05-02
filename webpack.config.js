546db4a6952e14112e9ef531556606dbf0583486
Graeme Hill <graemekh@gmail.com>
2018-05-01 22:58:28 -0700

    Fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
             { 
                 test: /\.jsx?$/, 
                 loader: 'babel-loader', 
-                exclude: /node_modules/ 
+                exclude: /node_modules/
             },
             {
                 test: /\.css$/,
