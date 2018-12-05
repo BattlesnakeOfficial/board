a714921a48bce9a74642cd2e837ba7555e96e2f2
Brandon Brown <brandon@brandonb.ca>
2018-12-04 22:39:55 -0800

    Optimize SVGs and adds some shape rendering attributes. I think using `optimizeSpeed` is a good compromise as it removes the need for the off-by-one pixel-rendering hack. It adds a loss of quality in the SVG (in some cases) but I think its a solid compromise.
    Also moves the scoreboard component out of the SVG so that it's much easier to style and update.

--- src/components/avatar.jsx
+++ src/components/avatar.jsx
@@ -2,59 +2,64 @@ import React from "react";
 import styled from "react-emotion";
 import { colors } from "../theme";
 
-const Name = styled("text")({
-  fontSize: "5px",
+const AvatarWrapper = styled("div")({
+  marginBottom: "1.5rem"
+});
+
+const Name = styled("span")({
+  display: "inline-block",
+  paddingBottom: ".5rem",
+  fontSize: "2.5rem",
+  fontWeight: 400,
   color: colors.darkText
 });
 
-const HealthBar = styled("rect")(({ color }) => ({
-  fill: color
-}));
-
-const HealthBarOutline = styled("rect")({
-  strokeWidth: 1,
-  stroke: colors.healthBarOutline
+const HealthBarOutline = styled("div")({
+  position: "relative",
+  width: "100%",
+  height: "3.5rem",
+  borderStyle: "solid",
+  borderWidth: "0.1rem",
+  borderColor: colors.healthBarOutline,
+  color: colors.white
 });
 
-const CauseOfDeath = styled("text")({
-  fontSize: "4px",
-  fill: colors.lightText
+const HealthBar = styled("div")(({ color }) => ({
+  height: "100%",
+  backgroundColor: color
+}));
+
+const CauseOfDeath = styled("div")({
+  height: "100%",
+  padding: "0.5rem",
+  fontSize: "1.8rem",
+  fontWeight: 400,
+  backgroundColor: colors.healthBarDeathBackground
 });
 
 class Avatar extends React.Component {
   render() {
-    const viewBoxWidth = 100;
-    const viewBoxHeight = 16;
-    const viewBox = `0 0 ${viewBoxWidth} ${viewBoxHeight}`;
     const healthWidthConversion = 96 / 100;
-    const healthWidth = this.props.snake.health * healthWidthConversion;
+    const healthWidth = Math.floor(
+      this.props.snake.health * healthWidthConversion
+    );
 
     return (
-      <svg
-        x={this.props.x}
-        y={this.props.y + 2}
-        viewBox={viewBox}
-        width={100}
-        height={16}
-      >
-        <Name x="2" y="6">
-          {this.props.snake.name}
-        </Name>
-        <HealthBarOutline x="2" y="8" width="96" height="6" />
-        {this.props.snake.death ? (
-          <CauseOfDeath x="4" y="12">
-            {this.props.snake.death.cause}
-          </CauseOfDeath>
-        ) : (
-          <HealthBar
-            x="2"
-            y="8"
-            width={healthWidth}
-            height="6"
-            color={this.props.snake.color}
-          />
-        )}
-      </svg>
+      <AvatarWrapper>
+        <Name>{this.props.snake.name}</Name>
+        <HealthBarOutline>
+          {this.props.snake.death ? (
+            <CauseOfDeath>{this.props.snake.death.cause}</CauseOfDeath>
+          ) : (
+            <HealthBar
+              color={this.props.snake.color}
+              style={{
+                width: `${healthWidth}%`
+              }}
+            />
+          )}
+        </HealthBarOutline>
+      </AvatarWrapper>
     );
   }
 }
