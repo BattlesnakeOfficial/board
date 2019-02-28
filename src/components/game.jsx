8e3e2e739582941e595142a58e24ce5c95359795
Aileen <aileen@sendwithus.com>
2019-02-28 09:09:44 -0800

    wip

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -16,7 +16,7 @@ const PageWrapper = styled("div")`
     theme === themes.dark ? colors.purple : "transparent"};
   background: ${({ theme }) =>
     theme === themes.dark
-      ? `linear-gradient(210deg,hsla(260, 89%, 16%, 1) 0%,hsla(277, 95%, 35%, 1) 100%)`
+      ? `linear-gradient(30deg,hsla(260, 89%, 16%, 1) 55%,hsla(277, 95%, 35%, 1) 100%)`
       : "transparent"};
 `;
 
