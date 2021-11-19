ffba8a85cb69efe7ba6505482c50461f58d5be58
Sherri Flemings <sherri@battlesnake.com>
2021-11-19 16:03:39 -0500

    Re-introducing the slider component with better styling and snap support.
    WIP

--- src/components/settings/playback/PlaybackSpeed.jsx
+++ src/components/settings/playback/PlaybackSpeed.jsx
@@ -1,10 +1,26 @@
+import style from "./PlaybackSpeed.module.css";
+
 const PlaybackSpeed = props => {
   return (
-    <select defaultValue={props.default} onChange={props.onChange}>
-      <option value={6}>Slow</option>
-      <option value={10}>Medium</option>
-      <option value={20}>Fast</option>
-    </select>
+    // <select defaultValue={props.default} onChange={props.onChange}>
+    //   <option value={6}>Slow</option>
+    //   <option value={10}>Medium</option>
+    //   <option value={20}>Fast</option>
+    // </select>
+    <div className="input-container">
+      <input
+        type="range"
+        min={1}
+        max={3}
+        value={props.default}
+        onChange={props.onChange}
+      />
+      <div className={style.labels}>
+        <span>Slow</span>
+        <span>Medium</span>
+        <span>Fast</span>
+      </div>
+    </div>
   );
 };
 
