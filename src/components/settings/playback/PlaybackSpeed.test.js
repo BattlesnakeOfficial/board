746da76ae8d3a577f9956896d3beba25c3617521
Sherri Flemings <sherri@battlesnake.com>
2021-12-09 19:53:11 -0500

    Added failing tests to cover updated speed values: SLOW: 3 MEDIUM: 6 FAST: 20

--- src/components/settings/playback/PlaybackSpeed.test.js
+++ src/components/settings/playback/PlaybackSpeed.test.js
@@ -1,13 +1,61 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 
-import PlaybackSpeed, { SLIDER_MEDIUM } from "./PlaybackSpeed";
+import PlaybackSpeed, {
+  SLIDER_FAST,
+  SLIDER_MEDIUM,
+  SLIDER_SLOW
+} from "./PlaybackSpeed";
 
-it("renders with or without a default speed", () => {
-  render(<PlaybackSpeed />);
+describe("renders with or without a default speed", () => {
+  it("uses medium as default when no value provided", () => {
+    render(<PlaybackSpeed />);
 
-  const speedSlider = screen.getByRole("slider");
+    const speedSlider = screen.getByRole("slider");
 
-  expect(speedSlider).toBeInTheDocument();
-  expect(speedSlider).toHaveValue(SLIDER_MEDIUM);
+    expect(speedSlider).toBeInTheDocument();
+    expect(speedSlider).toHaveValue(String(SLIDER_MEDIUM));
+  });
+
+  it("renders the supplied value as default", () => {
+    render(<PlaybackSpeed default={1} />);
+
+    const speedSlider = screen.getByRole("slider");
+
+    expect(speedSlider).toHaveValue(String(SLIDER_SLOW));
+  });
+
+  it("uses a default within range if supplied value is larger than max range", () => {
+    render(<PlaybackSpeed default={30} />);
+
+    const speedSlider = screen.getByRole("slider");
+
+    expect(Number(speedSlider.value)).toBeGreaterThanOrEqual(SLIDER_SLOW);
+    expect(Number(speedSlider.value)).toBeLessThanOrEqual(SLIDER_FAST);
+  });
+
+  it("uses a default within range if supplied value is less than min range", () => {
+    render(<PlaybackSpeed default={-1} />);
+
+    const speedSlider = screen.getByRole("slider");
+
+    expect(Number(speedSlider.value)).toBeGreaterThanOrEqual(SLIDER_SLOW);
+    expect(Number(speedSlider.value)).toBeLessThanOrEqual(SLIDER_FAST);
+  });
+});
+
+describe("renders the correct speed", () => {
+  test.each([
+    [1, SLIDER_SLOW],
+    [5, SLIDER_SLOW],
+    [6, SLIDER_MEDIUM],
+    [12, SLIDER_MEDIUM],
+    [13, SLIDER_FAST],
+    [20, SLIDER_FAST]
+  ])("framerate: %i", (fps, expected) => {
+    render(<PlaybackSpeed default={fps} />);
+    const speedSlider = screen.getByRole("slider");
+
+    expect(speedSlider).toHaveValue(String(expected));
+  });
 });
