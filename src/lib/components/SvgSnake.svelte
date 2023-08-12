e345c6384175db1c9d4da8b7ae9626f2ef3eef7e
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2023-08-12 15:40:59 +0000

    Fix render bug when snake collides with self. Also refactor body render.

--- src/lib/components/SvgSnake.svelte
+++ src/lib/components/SvgSnake.svelte
@@ -11,13 +11,37 @@
 	export let svgCalcParams: SvgCalcParams;
 	export let displayClass = '';
 
-	function displayHead(snake: Snake): boolean {
-		// Snake heads are always shown
-		return true;
+	$: drawHead = calcDrawHead(snake);
+	function calcDrawHead(snake: Snake): boolean {
+		return true; // Snake heads are always drawn!
 	}
 
-	function displayTail(snake: Snake): boolean {
-		// Snake heads take priority
+	$: headRectProps = svgCalcCellRect(svgCalcParams, snake.body[0]);
+
+	$: headTransform = calcHeadTransform(snake);
+	function calcHeadTransform(snake: Snake): string {
+		const [head, neck] = snake.body.slice(0, 2);
+
+		// Return transform based on relative position between neck and tail.
+		// Note that if the neck and tail overlap, we return the default (right).
+		// This is intended.
+		if (head.x < neck.x) {
+			// Moving left
+			return 'scale(-1,1) translate(-100, 0)';
+		} else if (head.y > neck.y) {
+			// Moving up
+			return 'rotate(-90, 50, 50)';
+		} else if (head.y < neck.y) {
+			// Moving down
+			return 'rotate(90, 50, 50)';
+		}
+		// Moving right
+		return '';
+	}
+
+	$: drawTail = calcDrawTail(snake);
+	function calcDrawTail(snake: Snake): boolean {
+		// Skip drawing the tail if the snake head occupies the same spot
 		const [head, tail] = [snake.body[0], snake.body[snake.body.length - 1]];
 		if (head.x == tail.x && head.y == tail.y) {
 			return false;
@@ -25,27 +49,64 @@
 		return true;
 	}
 
-	function svgPolylinePropsForSnakeBody(body: Point[]) {
+	$: tailRectProps = svgCalcCellRect(svgCalcParams, snake.body[snake.body.length - 1]);
+
+	$: tailTransform = calcTailTransform(snake);
+	function calcTailTransform(snake: Snake): string {
+		const tail = snake.body[snake.body.length - 1];
+
+		// Work backwards from the tail until we reach a segment that isn't stacked.
+		let preTailIndex = snake.body.length - 2;
+		let preTail = snake.body[preTailIndex];
+		while (preTail.x == tail.x && preTail.y == tail.y) {
+			preTailIndex -= 1;
+			if (preTailIndex < 0) {
+				return '';
+			}
+			preTail = snake.body[preTailIndex];
+		}
+
+		// Return transform based on relative location
+		if (preTail.x > tail.x) {
+			// Moving right
+			return 'scale(-1,1) translate(-100,0)';
+		} else if (preTail.y > tail.y) {
+			// Moving up
+			return 'scale(-1,1) translate(-100,0) rotate(90, 50, 50)';
+		} else if (preTail.y < tail.y) {
+			// Moving down
+			return 'scale(-1,1) translate(-100,0) rotate(-90, 50, 50)';
+		}
+		// Moving left
+		return '';
+	}
+
+	$: bodyPolylineProps = calcBodyPolylineProps(snake);
+	function calcBodyPolylineProps(snake: Snake) {
+		// Polyline drawing params
 		const strokeWidth = svgCalcParams.cellSize;
 		const strokeLinejoin = 'round';
 		let strokeLinecap = 'square';
 
-		const [head, tail] = [body[0], body[body.length - 1]];
+		// Make a copy of snake body and separate into head, tail, and body.
+		const body: Point[] = [...snake.body];
+		const head = body.shift() as Point;
+		const tail = body.pop() as Point;
 
-		const bodyCenterPoints = [];
-		for (let i = 0; i < body.length; i++) {
-			const p = body[i];
-
-			// Ignore points that overlap with the head and tail
-			if (head.x == p.x && head.y == p.y) {
-				continue;
-			} else if (tail.x == p.x && tail.y == p.y) {
-				continue;
-			}
-
-			const [cx, cy] = svgCalcCellCenter(svgCalcParams, p);
-			bodyCenterPoints.push({ cx: cx, cy: cy, x: p.x, y: p.y });
+		// Don't render any body parts that are stacked on the tail
+		while (
+			body.length > 0 &&
+			body[body.length - 1].x == tail.x &&
+			body[body.length - 1].y == tail.y
+		) {
+			body.pop();
 		}
+
+		// Get the center point of each body square we're going to render
+		const bodyCenterPoints = body.map((p) => {
+			const [cx, cy] = svgCalcCellCenter(svgCalcParams, p);
+			return { cx: cx, cy: cy, x: p.x, y: p.y };
+		});
 
 		// If we're drawing *any* body, we want to extend the first and last points
 		// to connect to the head and tail across the cell spacing.
@@ -68,13 +129,13 @@
 			// Extend last point towards tail
 			const last = bodyCenterPoints[bodyCenterPoints.length - 1];
 			if (tail.x > last.x) {
-				bodyCenterPoints.push({ cx: last.cx + gapSize, cy: last.cy });
+				bodyCenterPoints.push({ cx: last.cx + gapSize, cy: last.cy, x: 0, y: 0 });
 			} else if (tail.x < last.x) {
-				bodyCenterPoints.push({ cx: last.cx - gapSize, cy: last.cy });
+				bodyCenterPoints.push({ cx: last.cx - gapSize, cy: last.cy, x: 0, y: 0 });
 			} else if (tail.y > last.y) {
-				bodyCenterPoints.push({ cx: last.cx, cy: last.cy - gapSize });
+				bodyCenterPoints.push({ cx: last.cx, cy: last.cy - gapSize, x: 0, y: 0 });
 			} else if (tail.y < last.y) {
-				bodyCenterPoints.push({ cx: last.cx, cy: last.cy + gapSize });
+				bodyCenterPoints.push({ cx: last.cx, cy: last.cy + gapSize, x: 0, y: 0 });
 			}
 		}
 
@@ -88,28 +149,56 @@
 			if (head.x != tail.x || head.y != tail.y) {
 				const [cx, cy] = svgCalcCellCenter(svgCalcParams, head);
 				if (head.x > tail.x) {
-					bodyCenterPoints.push({ cx: cx - svgCalcParams.cellSizeHalf + OVERLAP, cy: cy });
+					bodyCenterPoints.push({
+						cx: cx - svgCalcParams.cellSizeHalf + OVERLAP,
+						cy: cy,
+						x: 0,
+						y: 0
+					});
 					bodyCenterPoints.push({
 						cx: cx - svgCalcParams.cellSizeHalf - svgCalcParams.cellSpacing - OVERLAP,
-						cy: cy
+						cy: cy,
+						x: 0,
+						y: 0
 					});
 				} else if (head.x < tail.x) {
-					bodyCenterPoints.push({ cx: cx + svgCalcParams.cellSizeHalf - OVERLAP, cy: cy });
+					bodyCenterPoints.push({
+						cx: cx + svgCalcParams.cellSizeHalf - OVERLAP,
+						cy: cy,
+						x: 0,
+						y: 0
+					});
 					bodyCenterPoints.push({
 						cx: cx + svgCalcParams.cellSizeHalf + svgCalcParams.cellSpacing + OVERLAP,
-						cy: cy
+						cy: cy,
+						x: 0,
+						y: 0
 					});
 				} else if (head.y > tail.y) {
-					bodyCenterPoints.push({ cx: cx, cy: cy + svgCalcParams.cellSizeHalf - OVERLAP });
 					bodyCenterPoints.push({
 						cx: cx,
-						cy: cy + svgCalcParams.cellSizeHalf + svgCalcParams.cellSpacing + OVERLAP
+						cy: cy + svgCalcParams.cellSizeHalf - OVERLAP,
+						x: 0,
+						y: 0
+					});
+					bodyCenterPoints.push({
+						cx: cx,
+						cy: cy + svgCalcParams.cellSizeHalf + svgCalcParams.cellSpacing + OVERLAP,
+						x: 0,
+						y: 0
 					});
 				} else if (head.y < tail.y) {
-					bodyCenterPoints.push({ cx: cx, cy: cy - svgCalcParams.cellSizeHalf + OVERLAP });
 					bodyCenterPoints.push({
 						cx: cx,
-						cy: cy - svgCalcParams.cellSizeHalf - svgCalcParams.cellSpacing - OVERLAP
+						cy: cy - svgCalcParams.cellSizeHalf + OVERLAP,
+						x: 0,
+						y: 0
+					});
+					bodyCenterPoints.push({
+						cx: cx,
+						cy: cy - svgCalcParams.cellSizeHalf - svgCalcParams.cellSpacing - OVERLAP,
+						x: 0,
+						y: 0
 					});
 				}
 			}
@@ -129,86 +218,29 @@
 			'stroke-linejoin': strokeLinejoin
 		};
 	}
-
-	function svgTransformForSnakeHead(snake: Snake): string {
-		const [head, neck] = snake.body.slice(0, 2);
-
-		// Return transform based on relative position between neck and tail.
-		// Note that if the neck and tail overlap, we return the default (right).
-		// This is intended.
-		if (head.x < neck.x) {
-			// Moving left
-			return 'scale(-1,1) translate(-100, 0)';
-		} else if (head.y > neck.y) {
-			// Moving up
-			return 'rotate(-90, 50, 50)';
-		} else if (head.y < neck.y) {
-			// Moving down
-			return 'rotate(90, 50, 50)';
-		}
-		// Moving right
-		return '';
-	}
-
-	function svgTransformForSnakeTail(snake: Snake): string {
-		const tailIndex = snake.body.length - 1;
-		const tail = snake.body[tailIndex];
-
-		// We need to work backwards from the tail until we reach a segment
-		// that doesn't overlap or is stacked.
-		let preTailIndex = tailIndex - 1;
-		let preTail = snake.body[preTailIndex];
-
-		while (preTail.x == tail.x && preTail.y == tail.y) {
-			preTailIndex -= 1;
-			preTail = snake.body[preTailIndex];
-		}
-
-		// Return transform based on relative location
-		if (preTail.x > tail.x) {
-			// Moving right
-			return 'scale(-1,1) translate(-100,0)';
-		} else if (preTail.y > tail.y) {
-			// Moving up
-			return 'scale(-1,1) translate(-100,0) rotate(90, 50, 50)';
-		} else if (preTail.y < tail.y) {
-			// Moving down
-			return 'scale(-1,1) translate(-100,0) rotate(-90, 50, 50)';
-		}
-		// Moving left
-		return '';
-	}
 </script>
 
 <g id={`snake-${snake.id}`} class="snake {displayClass}">
-	<!-- Body -->
-	<polyline stroke={snake.color} fill="transparent" {...svgPolylinePropsForSnakeBody(snake.body)} />
-
-	<!-- Head -->
-	{#await fetchCustomizationSvgDef('head', snake.head) then headSvgDef}
-		{#if displayHead(snake)}
-			<svg
-				viewBox="0 0 100 100"
-				fill={snake.color}
-				{...svgCalcCellRect(svgCalcParams, snake.body[0])}
-			>
-				<g transform={svgTransformForSnakeHead(snake)}>
-					{@html headSvgDef}
+	<!-- Tail -->
+	{#await fetchCustomizationSvgDef('tail', snake.tail) then tailSvgDef}
+		{#if drawTail}
+			<svg class="tail" viewBox="0 0 100 100" fill={snake.color} {...tailRectProps}>
+				<g transform={tailTransform}>
+					{@html tailSvgDef}
 				</g>
 			</svg>
 		{/if}
 	{/await}
 
-	<!-- Tail -->
-	{#await fetchCustomizationSvgDef('tail', snake.tail) then tailSvgDef}
-		{#if displayTail(snake)}
-			<svg
-				viewBox="0 0 100 100"
-				fill={snake.color}
-				{...svgCalcCellRect(svgCalcParams, snake.body[snake.body.length - 1])}
-			>
-				<g transform={svgTransformForSnakeTail(snake)}>
-					{@html tailSvgDef}
+	<!-- Body -->
+	<polyline stroke={snake.color} fill="transparent" {...bodyPolylineProps} />
+
+	<!-- Head -->
+	{#await fetchCustomizationSvgDef('head', snake.head) then headSvgDef}
+		{#if drawHead}
+			<svg class="head" viewBox="0 0 100 100" fill={snake.color} {...headRectProps}>
+				<g transform={headTransform}>
+					{@html headSvgDef}
 				</g>
 			</svg>
 		{/if}
