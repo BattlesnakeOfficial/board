fbfb7c13f9317d0ae7ce01f8ac63ba513a3e85fe
bvanvugt <1531419+bvanvugt@users.noreply.github.com>
2023-08-27 19:29:25 +0000

    Update comments

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,4 @@
-// We're a strict SPA, disable SSR site-wide
+// We're a strictly static site, disable SSR site-wide and force all pages to prerender.
+// See: https://kit.svelte.dev/docs/adapter-static
 export const prerender = true;
 export const ssr = false;
